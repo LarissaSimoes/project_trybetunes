a666111ce894997d3093d181fb67f14b69f27f38
LarissaSimoes <larissacarneiro2@hotmail.com>
2023-02-06 19:18:41 -0300

    Requisito 1

---
+++ src/pages/NotFound.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+class PageNotFound extends React.Component {
+  render() {
+    return (
+      <div data-testid="page-not-found">Page Not Found</div>
+    );
+  }
+}
+
+export default PageNotFound;
