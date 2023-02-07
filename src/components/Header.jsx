4c12e15ff8cd1dfcdda137098887f141fe4bc204
LarissaSimoes <larissacarneiro2@hotmail.com>
2023-02-07 15:41:44 -0300

    4

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 import Loading from './Loading';
 import { getUser } from '../services/userAPI';
 
@@ -22,9 +23,22 @@ class Header extends React.Component {
 
     return (
       <header data-testid="header-component">
+        <Link to="/search" data-testid="link-to-search">
+          Pesquisar
+        </Link>
+        <Link to="/favorites" data-testid="link-to-favorites">
+          Favoritas
+        </Link>
+        <Link to="/profile" data-testid="link-to-profile">
+          Favoritas
+        </Link>
         Header
         { isLoading ? (<Loading />) : null }
-        <p data-testid="header-user-name">{ name }</p>
+        <p data-testid="header-user-name">
+          Olá
+          { ' ' }
+          { name }
+        </p>
       </header>
     );
   }
