ca8f75a4b6cd75dc59d7fd765828d1620ad15c12
LarissaSimoes <larissacarneiro2@hotmail.com>
2023-02-08 19:53:30 -0300

    6

--- src/pages/Search.js
+++ src/pages/Search.js
@@ -1,11 +1,17 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 import Header from '../components/Header';
+import Loading from '../components/Loading';
+import searchAlbumsAPI from '../services/searchAlbumsAPI';
 
 const MIN_LENGTH = 2;
 class Search extends React.Component {
   state = {
     isButtonDisabled: true,
     search: '',
+    isLoading: false,
+    albums: [],
+    prevSearch: '',
   };
 
   enableButton = () => {
@@ -22,8 +28,23 @@ class Search extends React.Component {
     }, this.enableButton);
   };
 
+  searchFunc = async (e) => {
+    e.preventDefault();
+    const { search } = this.state;
+    this.setState({
+      isLoading: true,
+      prevSearch: search,
+    });
+    const albumsResp = await searchAlbumsAPI(search);
+    this.setState({
+      isLoading: false,
+      search: '',
+      albums: albumsResp,
+    });
+  };
+
   render() {
-    const { isButtonDisabled, search } = this.state;
+    const { isButtonDisabled, search, isLoading, albums, prevSearch } = this.state;
 
     return (
       <div data-testid="page-search">
@@ -38,15 +59,47 @@ class Search extends React.Component {
               placeholder="Nome do artista"
               value={ search }
               onChange={ this.handleChange }
+              id="search"
             />
           </label>
           <button
             data-testid="search-artist-button"
             disabled={ isButtonDisabled }
+            onClick={ this.searchFunc }
+            type="submit"
           >
             Pesquisar
           </button>
+          { isLoading ? (<Loading />) : null }
         </form>
+        {prevSearch && (
+
+          <h2>
+            Resultado de álbuns de:
+            {' '}
+            {prevSearch}
+          </h2>
+        )}
+        {albums.length === 0 ? (
+          <h2>Nenhum álbum foi encontrado</h2>
+        ) : (
+          <div>
+            {
+              albums.map((album, index) => (
+                <div key={ index }>
+                  <Link
+                    to={ `/album/${album.collectionId}` }
+                    data-testid={ `link-to-album-${album.collectionId}` }
+                  >
+                    <img src={ album.artworkUrl100 } alt={ album.artistName } />
+                    <p>{ album.collectionName }</p>
+                    <span>{ album.artistName }</span>
+                  </Link>
+                </div>
+              ))
+            }
+          </div>
+        )}
       </div>
     );
   }
